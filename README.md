e89a737a9e15ace8b4aece46c65b0d490cb1b26f
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2022-10-08 15:50:14 +0200

    Add script and readme

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # mullvad-relay-change
+
+Wrapper utility script to sequentially select Mullvad servers/countries from a custom pool
+
+## Country and server
+
+Country and server constraints can be chosen with command line arguments
+
+From the available servers and countries, only the ones that fit the constraints are selected to be in the final pool from which the next relay location/server will be extracted and eventually set using the `mullvad` commands
+
+To select countries, use the argument `--countries` followed by a list of countries you want in your constraints. For example:
+
+    $ python3 mullvadRelayChange.py --countries us pt it
+    Changing location to us
+    Relay constraints updated
+
+Invalid country codes will be ignored
+
+To select servers, use the argument `--servers` followed by a list of servers you want in your constraints.
+
+    $ python3 mullvadRelayChange.py --servers us258-wireguard pt1-wireguard fi3-wireguard us-sea-wg-001
+    Changing server to fi3-wireguard
+    Setting location constraint to fi3-wireguard in hel, fi
+    Relay constraints updated
+
+Invalid server names will be ignored
+
+The two arguments can be combined, so that in the final pool will only be the servers that also fit the country constraints. For example:
+
+    $ python3 mullvadRelayChange.py --servers us258-wireguard pt1-wireguard fi3-wireguard us-sea-wg-001 --countries us pt it
+    Changing server to us258-wireguard
+    Setting location constraint to us258-wireguard in den, us
+    Relay constraints updated
+
+In this case, the final pool will only be composed of the following servers:
+
+    pt1-wireguard us258-wireguard us-sea-wg-001
+
+... as these are the only ones that also fit the country constraints
+
+Without any argument the default behaviour is selecting a pool full of all the available countries
+
+When only countries are specified, the Mullvad app automatically chooses a server from the country to which the location has been changed (as far as I know, this is random, but I could be wrong)
+
+## Other utilities
+
+To facilitate the creation of custom pools, the script also features a listing function, which can print all the currently available countries, cities or servers
+
+    $ python3 mullvadRelayChange.py --print servers
+    Available servers:
+    al-tia-001
+    au-adl-001
+    [...]
+    us238-wireguard
+    us241-wireguard
+    $ python3 mullvadRelayChange.py --print cities
+    Available cities:
+    tia
+    adl
+    [...]
+    sea
+    uyk
+    $ python3 mullvadRelayChange.py --print countries
+    Available countries:
+    al
+    au
+    [...]
+    ae
+    us
+
+Note that right now cities can not be included in the script constraints.
+
+Finally, to make sure the right countries and/or servers are being selected, you can use the `--verbose` argument to make the script show the available countries and servers after the filtering done using the selected constraints
+
+    $ python3 mullvadRelayChange.py --servers us258-wireguard pt1-wireguard fi3-wireguard us-sea-wg-001 --countries us pt it --verbose
+    Changing server to us-sea-wg-001
+    Setting location constraint to us-sea-wg-001 in sea, us
+    Relay constraints updated
+    
+    Available countries given the current constraints:
+    it pt us
+    Available servers given the current constraints:
+    pt1-wireguard us258-wireguard us-sea-wg-001
+
+Note that currently offline servers are automatically filtered out because they are not shown in the output of the `mullvad relay list` command, which is used by the script to fetch the necessary data
---
+++ mullvadRelayChange.py
@@ -0,0 +1,183 @@
+#!/bin/python3
+
+import subprocess as sp
+from random import randint
+import sys
+
+def parseCountry(countryLine):
+    if "(" not in countryLine: return ""
+
+    countryLine = countryLine[countryLine.index("(") + 1:len(countryLine) - 1]
+    return countryLine
+
+def parseCity(cityLine):
+    if not ("(" in cityLine and ")" in cityLine): return ""
+
+    cityLine = cityLine[cityLine.index("(") + 1:cityLine.index(")")]
+    return cityLine
+
+def parseServer(serverLine):
+    if("\t\t" not in serverLine): return ""
+    serverLine = serverLine[serverLine.index("\t\t") + 2: len(serverLine)]
+    return serverLine.split(" ")[0]
+
+def countryFromServer(server):
+    firstToken = list(server.split("-")[0])
+    return "".join(c for c in firstToken if c not in "0123456789")
+
+def fetchServerData():
+    relays = sp.run(["mullvad", "relay", "list"], capture_output = True)
+    relays = relays.stdout.decode().splitlines()
+    countries = []
+    cities = []
+    servers = []
+    for i in range(0, len(relays)):
+        if(len(relays[i]) < 1): continue
+
+        fc = relays[i][0]
+        if(len(relays[i]) > 1): sc = relays[i][1]
+
+        if(fc != "\t"):
+            countries.append(parseCountry(relays[i]))
+        elif(len(relays[i]) < 2): continue
+        elif(fc == "\t" and sc != "\t"):
+            cities.append(parseCity(relays[i]))
+        elif(fc == "\t" and sc == "\t"):
+            servers.append(parseServer(relays[i]))
+    
+    countries = list(filter(lambda s: s != "", countries))
+    cities = list(filter(lambda s: s != "", cities))
+    servers = list(filter(lambda s: s != "", servers))
+    return countries, cities, servers
+
+def getCurrentCountry():
+    status = sp.run(["mullvad", "relay", "get"], capture_output = True)
+    status = status.stdout.decode().lower().split(" ")
+    if "country" not in status: return "/"
+    if status.index("country") + 1 >= len(status): return "/"
+
+    return status[status.index("country") + 1]
+
+def getCurrentServer():
+    status = sp.run(["mullvad", "status"], capture_output = True)
+    status = status.stdout.decode().lower().split(" ")
+
+    if "connected" not in status: return "/"
+    if status.index("to") + 1 >= len(status): return "/"
+
+    return status[status.index("to") + 1]
+
+def printList(list, label, endChar = "\n"):
+    print(f"Available {label}:", end = "\n")
+    if(len(list) == 1):
+        print(list[0])
+        return
+
+    for i in range(0, len(list) - 1):
+        print(list[i], end = endChar)
+    print(list[i + 1])
+
+def perror(errorMsg):
+    print(errorMsg, file = sys.stderr)
+
+def handleConstraints(args, argsIndex, constraints, mainArgs):
+    i = argsIndex
+    while(i < len(args) and args[i] not in mainArgs):
+        constraints.append(args[i])
+        i += 1
+
+    return i - 1
+
+mainArgs = ("--print", "--countries", "--servers", "--verbose")
+
+countries, cities, servers = fetchServerData()
+
+countryConstraints = []
+serverConstraints = []
+
+verbose = False
+
+i = 1
+while i < len(sys.argv):
+    arg = sys.argv[i]
+    if arg == "--print":
+        if len(sys.argv) <= i + 1:
+            perror("No option after --print\nAvailable options: countries, cities, servers")
+            sys.exit(1)
+
+        nextArg = sys.argv[i + 1]
+        if nextArg == "countries":
+            printList(countries, "countries")
+            quit()
+        elif nextArg == "cities":
+            printList(cities, "cities")
+            quit()
+        elif nextArg == "servers":
+            printList(servers, "servers")
+            quit()
+        else:
+            perror("Unrecognized option after --print\nAvailable options: countries, cities, servers")
+            sys.exit(1)
+    elif arg == "--countries":
+        if len(sys.argv) <= i + 1:
+            perror("No option(s) after --countries")
+            sys.exit(1)
+
+        i = handleConstraints(sys.argv, i + 1, countryConstraints, mainArgs)
+    elif arg == "--servers":
+        if len(sys.argv) <= i + 1:
+            perror("No option(s) after --servers")
+            sys.exit(1)
+
+        i = handleConstraints(sys.argv, i + 1, serverConstraints, mainArgs)
+    elif arg == "--verbose":
+        verbose = True
+    else:
+        perror(f"Unrecognized argument: {arg}")
+        sys.exit(1)
+
+    i += 1
+
+currentCountry = getCurrentCountry()
+currentServer = getCurrentServer()
+newCountryIndex = -1
+
+if countryConstraints != []:
+    availableCountries = list(filter(lambda c: c in countryConstraints, countries))
+    if availableCountries == []:
+        perror("No available countries amongst the ones specified")
+        sys.exit(1)
+else: availableCountries = countries
+
+if serverConstraints != []:
+    availableServers = list(filter(lambda s: s in serverConstraints and countryFromServer(s) in availableCountries, servers))
+    if availableServers == []:
+        perror("No compatible and available servers amongst the ones specified")
+        sys.exit(1)
+else: availableServers = []
+
+del countries
+del servers
+
+if availableServers == []:
+    if currentCountry not in availableCountries:
+        newCountryIndex = randint(0, len(availableCountries) - 1)
+    else:
+        newCountryIndex = (availableCountries.index(currentCountry) + 1) % len(availableCountries)
+
+    print(f"Changing location to {availableCountries[newCountryIndex]}")
+    sp.run(["mullvad", "relay", "set", "location", availableCountries[newCountryIndex]])
+else:
+    if currentServer not in availableServers:
+        newServerIndex = randint(0, len(availableServers) - 1)
+    else:
+        newServerIndex = (availableServers.index(currentServer) + 1) % len(availableServers)
+    print(f"Changing server to {availableServers[newServerIndex]}")
+    sp.run(["mullvad", "relay", "set", "hostname", availableServers[newServerIndex]])
+
+if verbose:
+    print(end = "\n")
+    printList(availableCountries, "countries given the current constraints", " ")
+    if(availableServers == []):
+        print("Available servers given the current constraints:\nAll servers in the available countries")
+    else: printList(availableServers, "servers given the current constraints", " ")
